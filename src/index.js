adca80770d4fbd0491f0ba4e3348580db628402b
GrigoriyNestsiarovich <79170403+GrigoriyNestsiarovich@users.noreply.github.com>
2021-03-07 23:27:06 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+  var res = "";
+    var num1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    var num2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    function propis(number){
+        switch (true){
+            case (number < 20): 
+            res = res + num1[number -1];
+            break;
+            case (number < 100):
+            res = num2[Math.floor(number / 10) - 2];
+            if (number % 10 != 0) res = res + ' ' + num1[number % 10 -1];
+            break;
+            case (number < 1000):
+                let arr = `${num1[Math.floor((number / 100 -1))]} hundred`;
+                if (number % 100 != 0) {
+                    res += `${arr} ${propis(number % 100)}`;
+                } else res = arr;
+                break;
+        }
+        return res;
+    }
+    res = res + propis(number);
+    return res;
 }
